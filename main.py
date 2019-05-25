70ccd2c7d5470eb85a0c79617f3531a911deefca
tz Y <2889205153@qq.com>
2019-05-25 12:56:02 +0800

    modified:   main.py 	new file:   static/avatars/0.png 	modified:   templates/portal.html 	new file:   validation.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template,redirect,url_for,session
+from flask import Flask,request,render_template,redirect,url_for,session,jsonify
 import flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
@@ -23,14 +23,25 @@ class User(db.Model):
     email = db.Column(db.String(80), nullable=True, unique=True)
     #role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(256), nullable=True)
+    kookies = db.Column(db.String(10),nullable=True,default='00000000')
+    admin = db.Column(db.Boolean,default=False)
     confirmed = db.Column(db.Boolean, default=False)
-    def __init__(self,username,password,email,password_hash,confirmed=False):
+    avatar = db.Column(db.Integer,default=0)
+    def __init__(self,username,password,email,password_hash,confirmed=False,kookies='00000000',admin=False,avatar=0):
         self.username = username
         self.password = password
         self.email = email
         self.password_hash = password_hash
+        self.kookies = kookies
+        self.admin = admin
         self.confirmed = confirmed
+        self.avatar = avatar
 
+
+def checklogin():
+    if session.get('username') != None:
+        return False
+    return True
 @app.route('/api/<kw>/')
 def api(kw):
     if kw == 'timestamp':
@@ -45,8 +56,19 @@ def home():
     return render_template('home.html',userip = ip,datetime=datetime.datetime.now(),loginform=loginform(),signinform=signinform())
 @app.route('/home',methods=['GET','POST'])
 def homepage():
-    username=session['username']
-    return render_template('portal.html',username=username)
+    if checklogin():
+        return redirect(url_for('home',nologin=1))
+    username=session.get('username')
+    result = User.query.filter(User.username == username).first()
+    kookies=result.kookies
+    avatar = result.avatar
+    avatarhref = '\static/avatars/'+str(avatar)+'.png'
+    return render_template('portal.html',username=username,kookies=kookies,avatarhref=avatarhref)
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home',logout=1))
+
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -62,6 +84,8 @@ def login():
             return redirect(url_for('home',err=1))
         elif result.password == password:
             session['username']=result.username
+            session['kookies']=result.kookies
+            session['avatar']=result.avatar
             return redirect(url_for('homepage'))
         else:
             return redirect(url_for('home',err=1))
@@ -83,7 +107,7 @@ def signin():
             print('密码与确认密码不同')
             return redirect(url_for('home',err=1))
         try:
-            newuser = User(username=username,password=psw1,email=email,password_hash=md5(psw1),confirmed=False)
+            newuser = User(username=username,password=psw1,email=email,password_hash=md5(psw1),confirmed=False,admin=False)
             db.session.add(newuser)
             db.session.commit()
             session['username']=username
